1fe24975825af3cd1570eb4c7cbaf4ffeaf8d662
brenda87 <79244433+brenda87@users.noreply.github.com>
2025-11-19 07:38:03 +0300

    Implement setup for Invariant contract with pools
    Initialize the Invariant contract with ERC20Mock tokens and a TSwapPool instance. Set up initial balances and approve tokens for the pool.

--- Invariant.t.sol
+++ Invariant.t.sol
@@ -4,10 +4,34 @@ pragma solidity 0.8.20;
 import { Test } from "forge-std/Test.sol";
 import { StdInvariant } from "forge-std/StdInvariant.sol";
 import { ERC20Mock } from "../mocks/ERC20Mock.sol";
+import {PoolFactory} from "../../src/PoolFactory.sol";
+import {TSwapPool} from "../../src/TSwapPool.sol";
 
 contract Invariant is StdInvariant, Test {
+    // these pools have 2 assets
     ERC20Mock poolToken;
     ERC20Mock weth;
 
-    function setUp() public { }
+    PoolFactory factory;
+    TSwapPool pool; // pool token / weth
+
+    int256 constant STARTING_X = 100e18;
+    int256 constant STARTING_Y = 50e18;
+
+    function setUp() public { 
+        weth = new ERC20Mock();
+        poolToken = new ERC20Mock();
+        factory = new PoolFactory(address(weth));
+        pool = TSwapPool(factory.createPool(address(poolToken)));
+        
+        // x and y balances 
+        poolToken.mint(address(this), uint256(STARTING_X));
+        weth.mint(address(this), uint256(STARTING_Y));
+
+        poolToken.approve(address(pool), type(uint256).max);
+        weth.approve(address(pool), type(uint256).max);
+
+        // deposit into pool
+        pool.deposit(uint256(STARTING_Y), uint256(STARTING_Y), uint256(STARTING_X), uint64(block.timestamp));
+    }
 }
