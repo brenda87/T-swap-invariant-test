ba0e58613142ff93e06ac5df2304e3bb3887d7cb
brenda87 <79244433+brenda87@users.noreply.github.com>
2025-11-19 17:52:19 +0300

    Refactor Invariant contract to include Handler and console2
    Added console2 for logging and integrated Handler contract.

--- Invariant.t.sol
+++ Invariant.t.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { Test } from "forge-std/Test.sol";
+import { Test, console2 } from "forge-std/Test.sol";
 import { StdInvariant } from "forge-std/StdInvariant.sol";
 import { ERC20Mock } from "../mocks/ERC20Mock.sol";
-import {PoolFactory} from "../../src/PoolFactory.sol";
-import {TSwapPool} from "../../src/TSwapPool.sol";
+import { PoolFactory } from "../../src/PoolFactory.sol";
+import { TSwapPool } from "../../src/TSwapPool.sol";
+import { Handler } from "./Handler.t.sol";
 
 contract Invariant is StdInvariant, Test {
     // these pools have 2 assets
@@ -15,16 +16,18 @@ contract Invariant is StdInvariant, Test {
     PoolFactory factory;
     TSwapPool pool; // pool token / weth
 
+    Handler handler;
+
     int256 constant STARTING_X = 100e18;
     int256 constant STARTING_Y = 50e18;
 
-    function setUp() public { 
+    function setUp() public {
         weth = new ERC20Mock();
         poolToken = new ERC20Mock();
         factory = new PoolFactory(address(weth));
         pool = TSwapPool(factory.createPool(address(poolToken)));
-        
-        // x and y balances 
+
+        // x and y balances
         poolToken.mint(address(this), uint256(STARTING_X));
         weth.mint(address(this), uint256(STARTING_Y));
 
@@ -33,5 +36,23 @@ contract Invariant is StdInvariant, Test {
 
         // deposit into pool
         pool.deposit(uint256(STARTING_Y), uint256(STARTING_Y), uint256(STARTING_X), uint64(block.timestamp));
+
+        handler = new Handler(pool);
+        bytes4[] memory selectors = new bytes4[](2);
+        selectors[0] = Handler.deposit.selector;
+        selectors[1] = Handler.swapPoolTokenForWethBasedOnOutputWeth.selector;
+
+        targetSelector(FuzzSelector({ addr: address(handler), selectors: selectors }));
+        targetContract(address(handler));
+    }
+
+    function statefulFuzz_constantProductFormulaStaysTheSameX() public view {
+        console2.log("actualDeltaX:", handler.actualDeltaX());
+        console2.log("expectedDeltaX:", handler.expectedDeltaX());
+        assertEq(handler.actualDeltaX(), handler.expectedDeltaX());
+    }
+
+    function statefulFuzz_constantProductFormulaStaysTheSameY() public view {
+        assertEq(handler.actualDeltaY(), handler.expectedDeltaY());
     }
 }
